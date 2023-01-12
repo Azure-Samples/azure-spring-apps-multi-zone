0af653c80809ac6c4d8aba59d6bb738395896edb
Gitte Vermeiren <givermei@microsoft.com>
2023-01-12 08:32:01 +0000

    removed nsg for ADF

--- tf-deploy/modules/network/main.tf
+++ tf-deploy/modules/network/main.tf
@@ -39,39 +39,3 @@ resource "azurerm_role_assignment" "asa_vnet_role_assignment" {
   role_definition_name = "Owner"
   principal_id = "d2531223-68f9-459e-b225-5592f90d145e"
 }
-
-
-resource "azurerm_network_security_group" "nsg-afd" {
-  name                = "OnlyAllowAFD"
-  location            = var.location
-  resource_group_name = var.resource_group
-
-  security_rule {
-    name                       = "allow-afd"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "AzureFrontDoor.Backend"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name                       = "allow-mgmt"
-    priority                   = 200
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "65200-65535"
-    source_address_prefix      = "GatewayManager"
-    destination_address_prefix = "*"
-  }
-
-}
-
-resource "azurerm_subnet_network_security_group_association" "appgw-nsg" {
-  subnet_id                 = azurerm_subnet.appgw_subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg-afd.id
-}
